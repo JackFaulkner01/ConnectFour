87b4928ebda3f5d777c468ca12c2d4e582358f0a
Jack Faulkner <jackjoshua.faulkner@gmail.com>
2022-11-09 20:34:16 +0000

    Basic connect four game, doesn't check for all wins

--- js/connectfour.js
+++ js/connectfour.js
@@ -1,102 +1,150 @@
-var discs = Array;
+var discs = [[0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]];
 var player1 = "discRed";
 var player2 = "discGreen";
 var isPlayer1 = true;
+var gameOver = false;
 
 function bodyOnLoad() {
+    var temps = document.getElementsByClassName("disc");
+    var tempColumn, tempRow;
 
-    discs = document.getElementsByClassName("disc");
+    for (var i = 0; i < temps.length; i++) {
+        temps[i].addEventListener("mouseover", hoverOverDisc);
+        temps[i].addEventListener("mouseout", hoverOutDisc);
+        temps[i].addEventListener("click", selectDisc);
+        tempColumn = parseInt(temps[i].id.charAt(4)) - 1;
+        tempRow = parseInt(temps[i].id.charAt(5)) - 1;
+        discs[tempRow][tempColumn] = temps[i];
+    }
+}
 
-    for (var i = 0; i < discs.length; i++) {
+function hoverOverDisc() {
+    if (gameOver || isSelected(this)) {
+        return;
+    }
+
+    var colour = getColour();
+    var disc = getLowestDisc(this);
+    
+    if (disc) {
+        disc.classList.add(colour + "Hover");
+    }
+}
+
+function hoverOutDisc() {
+    if (gameOver || isSelected(this)) {
+        return;
+    }
+
+    var colour = getColour();
+    var disc = getLowestDisc(this);
+
+    if (disc) {
+        disc.classList.remove(colour + "Hover");
+    }
+}
+
+function selectDisc() {
+    if (gameOver || isSelected(this)) {
+        return;
+    }
+
+    var colour = getColour();
+    var disc = getLowestDisc(this);
+
+    if (disc) {
+        disc.classList.remove(colour + "Hover");
+        disc.classList.add(colour);
+        checkWon(disc);
         
-        discs[i].addEventListener("mouseover", hoverOverDisc);
-        discs[i].addEventListener("mouseout", hoverOutDisc);
-        discs[i].addEventListener("click", selectDisc);
+        if (isPlayer1) {
+            isPlayer1 = false;
+        } else {
+            isPlayer1 = true;
+        }
     }
 }
 
 function isSelected(disc) {
-
     return disc.classList.contains(player1) || disc.classList.contains(player2);
 }
 
 function getColour() {
-
     if (isPlayer1) {
-
         return player1;
     }
 
     return player2;
 }
 
-function getLowestDisc(disc) {
+function getNotColour() {
+    if (isPlayer1) {
+        return player2;
+    }
 
-    var column = disc.id.charAt(4);
-    var row = 8;
+    return player1;
+}
+
+function getLowestDisc(disc) {
+    var column = disc.id.charAt(4) - 1;
+    var row = 6;
     var lowestDisc = null;
 
-    for (var i = 0; i < discs.length; i++) {
-
-        var discColumn = parseInt(discs[i].id.charAt(4));
-        var discRow = parseInt(discs[i].id.charAt(5));
-
-        if (discColumn == column && discRow < row && !isSelected(discs[i])) {
-
-            row = discRow;
-            lowestDisc = discs[i];
+    for (var i = 0; i < 6; i++) {
+        if (i < row && !isSelected(discs[i][column])) {
+            row = i;
+            lowestDisc = discs[i][column];
+            break;
         }
     }
 
     return lowestDisc;
 }
 
-function hoverOverDisc() {
+function checkWon(disc) {
+    var allSteps = [[1, 0], [-1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
 
-    if (isSelected(this)) {
+    for (var i = 0; i < allSteps.length; i++) {
+        if (scan(disc, allSteps[i])) {
+            for (var column = 0; column < 7; column++) {
+                for (var row = 0; row < 6; row++) {
+                    discs[row][column].classList.remove(getNotColour());
+                    discs[row][column].classList.add(getColour());
+                }
+            }
 
-        return;
-    }
-
-    var colour = getColour();
-    var disc = getLowestDisc(this);
-    
-    if (disc) {
-    
-        disc.classList.add(colour + "Hover");
-    }
-}
-
-function hoverOutDisc() {
-
-    if (isSelected(this)) {
-
-        return;
-    }
-
-    var colour = getColour();
-    var disc = getLowestDisc(this);
-
-    if (disc) {
-    
-        disc.classList.remove(colour + "Hover");
+            gameOver = true;
+            return;
+        }
     }
 }
 
-function selectDisc() {
+function scan(disc, step) {
+    var column = disc.id.charAt(4) - 1;
+    var row = disc.id.charAt(5) - 1;
+    var count = 1;
 
-    if (isSelected(this)) {
+    while (true) {
+        column += step[0];
+        row += step[1];
 
-        return;
-    }
+        if (column < 0 || column > 6 || row < 0 || row > 5) {
+            return false;
+        }
+        console.log(column + "," + row);
+        if (discs[row][column].classList.contains(getColour())) {
+            count++;
 
-    var colour = getColour();
-    var disc = getLowestDisc(this);
-
-    if (disc) {
-    
-        disc.classList.remove(colour + "Hover");
-        disc.classList.add(colour);
-        isPlayer1 = !isPlayer1;
+            if (count == 4) {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
